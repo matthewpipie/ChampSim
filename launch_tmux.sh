edaf5f76e23f34cffa9e5c5dcfd84673c0f8bb01
Matthew Giordano <matthewpipie@gmail.com>
2025-09-04 21:35:58 +0000

    launch spec

--- launch_tmux.sh
+++ launch_tmux.sh
@@ -12,9 +12,9 @@ for fil in $(ls bin/); do
 		tmux_name=$(date +%s_%N)
 		tmux new-session -d -s $tmux_name
 		if echo $fil | grep core.4; then
-			tmux send-keys -t "$tmux_name" Enter Enter "python3 run_google_on.py bin/$fil $numcores"
+			tmux send-keys -t "$tmux_name" Enter Enter "python3 run_spec_on.py bin/$fil $numcores"
 		else
-			tmux send-keys -t "$tmux_name" Enter Enter "python3 run_google_on1.py bin/$fil $numcores"
+			tmux send-keys -t "$tmux_name" Enter Enter "python3 run_spec_on1.py bin/$fil $numcores"
 		fi
 	fi
 done
